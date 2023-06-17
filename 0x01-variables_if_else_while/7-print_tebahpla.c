18a3e6ce655e1323b4b7c7dc46b49d8dd64a8163
wintubu <bunwinny@gmail.com>
2023-06-17 13:34:11 -0700

    7-print_tebahpla.c tasks

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
 
-
 /**
-
  * main - prints the small letters alphabet in reverse,
-
  * followed by a new line
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
+	char ch;
 
-        char ch;
-
-
-        for (ch = 'z'; ch >= 'a'; ch--)
-
-        {
-
-                putchar(ch);
-
-        }
-
-        putchar('\n');
-
-        return (0);
-
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
 
