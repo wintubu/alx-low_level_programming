ea3353d067cccf07dc5e595d41134a1fa688fab3
wintubu <bunwinny@gmail.com>
2023-06-17 13:00:00 -0700

    4-print_alphabt.c tas

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,44 +1,25 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-
 /**
-
  * main - Returns  the alphabet without q and e.
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
+	int i;
 
-        int i;
-
-
-        for (i = 97; i < 123; i++)
-
-        {
-
-                if (i != 101 && i != 113)
-
-                {
-
-                        putchar(i);
-
-                }
-
-        }
-
-        putchar('\n');
-
-        return (0);
-
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
