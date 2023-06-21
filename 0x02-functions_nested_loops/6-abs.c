c069e48c817e4bbce5f7a0e7e8317fcf8376d430
wintubu <bunwinny@gmail.com>
2023-06-21 12:08:34 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an integer
+* @nu: the int to check
+* Return: the absolute value of int
+*/
+int _abs(int nu)
+
+{
+	if (nu >= 0)
+	{
+		return (nu);
+	}
+	return (-nu);
+}
