d332ee2d8f2dccb04a6da7ae0f09a9ba6578444b
wintubu <bunwinny@gmail.com>
2023-06-26 10:54:35 -0700

    resetting integer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function with one argument
+ * @a: intger ype pointer a
+ *
+ * Description: using pointer as parameter to update value it points to 98
+ * Return: value
+ */
+void reset_to_98(int *a)
+{
+	*a = 98;
+}
