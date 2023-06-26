b136041b67e380319abdcd39ceb403107777186c
wintubu <bunwinny@gmail.com>
2023-06-26 11:56:20 -0700

    prints string to stdio

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - functionfunction that prints a string, followed by a new line, to stdout
+ * @strng: the string to print
+ *
+ * Description: print a string follow by a new line to stdout
+ * Return: none
+ */
+void _puts(char *str)
+{
+	while (*strng)
+	{
+		_putchar(*strng);
+		strng++;
+	}
+	_putchar('\n');
+}
