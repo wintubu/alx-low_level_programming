7427d598474fcc0a28a7a4f844023c7944907a3c
wintubu <bunwinny@gmail.com>
2023-06-26 11:22:38 -0700

    swap integer position

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * swap_int - functin having  two int type pointer argument
- * @ x: 1sinteger to be swapped
- * @y: 2nd argument integer to be swapped
- * Description: Using pointer to change value of x and y
- * Return: void
+ *swap_int - functin having  two int type pointer argument
+ *@x: 1sinteger to be swapped
+ *@y: 2nd argument integer to be swapped
+ *Description: Using pointer to change value of x and y
+ *Return: void
  */
 void swap_int(int *x, int *y)
 {
