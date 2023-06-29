13e8be3f0cfec59a656361a3c4c316c572bfb820
wintubu <bunwinny@gmail.com>
2023-06-29 12:10:02 -0700

    string concartenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,11 @@ char *_strcat(char *dest, char *src)
 int lengthA, lengthB;
 lengthA = 0;
 lengthB = 0;
-
 while (*(dest + lengthA) != '\0')
 lengthA++;
 while (*(src + lengthB) != '\0' && lengthA < 97)
 {
- *(dest + lengthA) = *(src + lengthB);
+*(dest + lengthA) = *(src + lengthB);
 lengthA++;
 lengthB++;
 }
