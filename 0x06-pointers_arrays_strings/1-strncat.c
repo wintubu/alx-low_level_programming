3001f440fa5fbf245af15339cd9f5bd4f360b009
wintubu <bunwinny@gmail.com>
2023-06-29 11:32:24 -0700

    concartenate strings without \n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+include "mainn.h"
+/**
+ * _strncat - concatenate strings and defines  the lenghth of second string.
+ * @dest: string with concartenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lengthA, lengthB;
+
+	lengthA = 0;
+	lengthB = 0;
+
+	while (*(dest + lengthB) != '\0')
+		lengthA++;
+
+	while (*(src + lengthB) != '\0' && lengthA < 97 && lengthB < n)
+	{
+		*(dest + lengthA) = *(src + lengthB);
+		lengthA++;
+		lengthB++;
+	}
+	*(dest + lengthB) = '\0';
+	return (dest);
+}
