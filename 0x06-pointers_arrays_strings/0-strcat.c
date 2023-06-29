5fee319afdce45e76ee6935c38573e384a8939b0
wintubu <bunwinny@gmail.com>
2023-06-29 11:22:03 -0700

    concartenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h>
+/**
+ * _strcat - concartenates first and second string
+ * @dest: string to be concatenated
+ * @src: string to be concatenated
+ * Return: Always 0.
+/ *
+char *_strcat(char *dest, char *src)
+{
+char *_strcat(char *dest, char *src)
+int lenghthA, lengthB;
+lengthA = 0;
+lenghtB = 0;
 
-int main () {
-
-char stra[10] = "God";
-char strb[12] = "First";
-
-strcat( stra, strb);
-printf("strcat( stra, strb): %s\n", stra );
-return 0;
+while (*(dest + lenghA) != '\0')
+lengthA++;
+while (*(src + lengthB) != '\0' && lengthA < 97)
+{
+ *(dest + lengthA) = *(src + lengthB);
+lengthA++;
+lengthB++;
+}
+*(dest + lengthA) = '\0';
+return (dest);
 }
