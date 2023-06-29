760b9c495b393ff22a611d3848ec76b86fe98e11
wintubu <bunwinny@gmail.com>
2023-06-29 12:42:32 -0700

    reverses string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - prints a  reversed array.
+ * @a: array to bereversedd.
+ * @n: size of array.
+ * Return: reversed array.
+ */
+void reverse_array(int *a, int n)
+{
+	int swap,start,last;
+
+	start = 0;
+	last = n - 1;
+	while (start < last)
+	{
+		swap = *(a + start);
+		*(a +start) = *(a + last);
+		*(a + last) = swap;
+		start++;
+		last--;
+	}
+}
