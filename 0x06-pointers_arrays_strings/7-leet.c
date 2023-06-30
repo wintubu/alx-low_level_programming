a2339f8401c4f49fa0ace2d2b24ff651ff77969b
wintubu <bunwinny@gmail.com>
2023-06-30 02:50:36 -0700

    converts letters to numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,27 @@
+#include "main.h"
+/**
+ * leet - change letter to numbers.
+ * @s:source  analized string.
+ *
+ * Return: String with all vowels changed.
+ */
+char *leet(char *s)
+{
+	char [e] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
 
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == e[j])
+			{
+				*(s + i) = n[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
