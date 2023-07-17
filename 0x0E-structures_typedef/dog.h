363146a51c12df2040bf07f2e79dc0093bcc95ec
wintubu <bunwinny@gmail.com>
2023-07-17 11:03:30 -0700

    description of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - descibes a pet
+ * @name: name
+ * @age: age
+ * @owner: dog owner
+ *
+ * Description: A Long description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
