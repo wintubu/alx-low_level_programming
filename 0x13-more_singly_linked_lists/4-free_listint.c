c5da9c61c87d5d04e958da0acb0584b0763100f0
wintubu <bunwinny@gmail.com>
2023-08-01 10:50:40 -0700

    frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ *free_list -Prototype of function that  Frees a list
+ *@head:Is a pointer to the list head
+ *
+ */
+
+void free_list(list_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
+}
