1258367f2bd9c5956a5bee7ab3643bb021ee44d6
wintubu <bunwinny@gmail.com>
2023-08-01 12:01:47 -0700

    free and reset the tail

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,17 @@
 #include "lists.h"
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	listint_t *last_item;
+	listint_t *LAST_ITEM;
 
-	while (head)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		last_item = head->next;
-		free(head);
-		head = last_item;
+		LAST_ITEM = (*head)->next;
+		free(*head);
+		*head = LAST_ITEM;
 	}
+
+	head = NULL;
 }
