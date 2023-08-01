ed84fcca4d2d00169691a5bb0c1a601466bd4cc4
wintubu <bunwinny@gmail.com>
2023-08-01 12:50:22 -0700

    get a nth elememt of a list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+include "lists.h"
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int a = 0;
+	listint_t *node;
+
+	if (head == NULL)
+			return (NULL);
+
+	node = head;
+	while (a < index)
+	{
+		if (!node)
+			return (NULL);
+		node = node->next;
+		a++;
+	}
+	return (node);
+}
+
+
